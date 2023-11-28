8e28b0480a573354eec631cb38e7c5050ea77773
mkharazishvili95 <mkharazishvili1@gmail.com>
2023-11-28 15:15:13 +0400

    Classes

---
+++ ClassesAndObjectsTask/Employee.cs
@@ -0,0 +1,35 @@
+﻿namespace ClassesAndObjectsTask
+{
+    public class employee
+    {
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public employee()
+        {
+            Age = 28;
+            Surname = "Kharazishvili";
+        }
+
+        public employee(string surname, int age)
+        {
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+        private string PrivateMethod()
+        {
+            return "Unknown Result";
+        }
+
+        public void SetSurname(string newSurname)
+        {
+            this.Surname = newSurname;
+        }
+
+        public string GetEmployeeInfo()
+        {
+            return $"Surname: {Surname}, Age: {Age}";
+        }
+    }
+}
